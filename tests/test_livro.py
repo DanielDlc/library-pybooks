ee9bcad8673d59986d607c081bdb04c51a02b273
DanielDlc <dlc.engcomputacao@gmail.com>
2025-02-04 18:44:18 +0000

    test livro

--- tests/test_livro.py
+++ tests/test_livro.py
@@ -1,17 +1,20 @@
 from src.livro import Livro
+from src.livro_digital import LivroDigital
 
+def test_comparar_livros():
+    livro1 = Livro("Python Fluente", "Luciano Ramalho")
+    livro2 = Livro("Python Fluente", "Luciano Ramalho")
+    livro3 = Livro("Clean Code", "Robert C. Martin")
 
-def test_criar_livro():
-    """Testa a criação de um livro com dados reais."""
+    assert livro1 == livro2  # São iguais
+    assert livro1 != livro3  # São diferentes
 
-    TITULO = "Clean Code"
-    AUTOR = "Robert C. Martin"
-    GENERO = "Programação"
-    ANO_ESPERADO = 2008
+def test_somar_livros_digitais():
+    livro1 = LivroDigital("Automate the Boring Stuff", "Al Sweigart", tamanho=5.5)
+    livro2 = LivroDigital("Python Crash Course", "Eric Matthes", tamanho=3.2)
 
-    livro = Livro(TITULO, AUTOR, GENERO, ANO_ESPERADO)
+    resultado = livro1 + livro2
 
-    assert livro.titulo == TITULO
-    assert livro.autor == AUTOR
-    assert livro.genero == GENERO
-    assert livro.ano == ANO_ESPERADO
+    assert resultado.titulo == "Automate the Boring Stuff + Python Crash Course"
+    assert resultado.autor == "Múltiplos Autores"
+    assert resultado.tamanho == 8.7  # Soma dos tamanhos dos arquivos
