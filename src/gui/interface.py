e7217d010c1c554d5f5732548865a06f2f5be309
DanielDlc <dlc.engcomputacao@gmail.com>
2025-02-04 21:34:11 +0000

    interface

---
+++ src/gui/interface.py
@@ -0,0 +1,32 @@
+"""Interface principal da aplicação."""
+
+import tkinter as tk
+from src.gui.livro_form import LivroForm
+from src.gui.livro_list import LivroList
+from src.managers.livro_manager import LivroManager
+
+class Interface:
+    """Janela principal do sistema de gerenciamento de livros."""
+
+    def __init__(self):
+        """Inicializa a interface principal."""
+        self.janela = tk.Tk()
+        self.janela.title('Sistema de Gerenciamento de Livraria')
+
+        # Criar o gerenciador de livros (Singleton)
+        self.livro_manager = LivroManager()
+
+        # Frame principal para a organização dos elementos
+        self.container = tk.Frame(self.janela)
+        self.container.pack(padx=20, pady=20)
+
+        # Criar o formulário de cadastro de livros
+        self.livro_form = LivroForm(self.container, self.livro_manager)
+
+        # Criar a lista de livros
+        self.livro_list = LivroList(self.container, self.livro_manager)
+
+        # Atualizar a lista de livros no início
+        self.livro_list.atualizar_lista()
+
+        self.janela.mainloop()
