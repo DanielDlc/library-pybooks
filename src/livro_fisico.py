0b0764d9b878bfc14f71c6eaf503715f511dc3ca
DanielDlc <dlc.engcomputacao@gmail.com>
2025-02-04 18:44:00 +0000

    livro fisico

--- src/livro_fisico.py
+++ src/livro_fisico.py
@@ -1,16 +1,27 @@
-from livro import Livro
+from src.livro import LivroComImagem
+import os
 
+IMG_DIR = os.path.join(os.path.dirname(__file__), "..", "img", "fisicos")
 
-class LivroFisico(Livro):
-    def __init__(self, titulo, autor, **kwargs):
-        super().__init__(titulo, autor)
-        self.genero = kwargs.get("genero")
-        self.ano = kwargs.get("ano")
-        self.localizacao = kwargs.get("localizacao")
-        self.condicao = kwargs.get("condicao", "novo")
+class LivroFisico(LivroComImagem):
+    def __init__(self, titulo, autor, localizacao=None, condicao="Novo", diretorio_imagem=None, **kwargs):
+        # Define o caminho padrão para a imagem se não for fornecido
+        if diretorio_imagem is None:
+            diretorio_imagem = os.path.join(IMG_DIR, "default.jpg")
+
+        # Armazena os atributos específicos do LivroFisico ANTES de chamar super()
+        self.localizacao = localizacao if localizacao else "Desconhecida"
+        self.condicao = condicao
+
+        # Remove "localizacao" e "condicao" de kwargs para evitar que cheguem em LivroComImagem
+        kwargs.pop("localizacao", None)
+        kwargs.pop("condicao", None)
+
+        # Chama o construtor da superclasse
+        super().__init__(titulo, autor, diretorio_imagem, **kwargs)
 
     def exibir_detalhes(self):
         detalhes = super().exibir_detalhes()
-        detalhes += f'\nLocalização: {self.localizacao or "Desconhecida"}'
-        detalhes += f'\nCondição: {self.condicao}'
+        detalhes += f'\n📍 Localização: {self.localizacao}'
+        detalhes += f'\n📌 Condição: {self.condicao}'
         return detalhes
