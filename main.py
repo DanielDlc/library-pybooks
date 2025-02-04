ea54976eea804bae92b1f6c6847b114559192aa6
DanielDlc <dlc.engcomputacao@gmail.com>
2025-02-04 21:32:13 +0000

    main

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-from src.interface_usuario import Interface
-from src.livraria import Livraria
+"""Arquivo principal da aplicação."""
 
-if __name__ == '__main__':
-    minha_livraria = Livraria()
-    minha_livraria.carregar_dados()
+from src.gui.interface import Interface
 
-    interface = Interface(minha_livraria)
-    interface.iniciar()
-
-    minha_livraria.salvar_dados()
+if __name__ == "__main__":
+    Interface()
