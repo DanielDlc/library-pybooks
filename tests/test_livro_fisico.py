7f1f6c90755c3748bd61d3d0a17f61589d46faca
DanielDlc <dlc.engcomputacao@gmail.com>
2025-02-04 09:41:21 +0000

    livro fisico test

---
+++ tests/test_livro_fisico.py
@@ -0,0 +1,16 @@
+from src.livro_fisico import LivroFisico
+
+
+def test_criar_livro_fisico():
+    """ Estou testando livro físico com dados reais"""
+    livro = LivroFisico(
+        "Python Fluente",
+        "Luciano Ramalho",
+        localizacao="Estande B",
+        condicao="Novo"
+        )
+
+    assert livro.titulo == "Python Fluente"
+    assert livro.autor == "Luciano Ramalho"
+    assert livro.localizacao == "Estande B"
+    assert livro.condicao == "Novo"
